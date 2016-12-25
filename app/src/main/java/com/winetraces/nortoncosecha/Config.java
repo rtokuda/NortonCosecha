65e02f7ade602854f2c8de521a9d45e8868811d5
RNT <roberto.tokuda@gmail.com>
2016-12-25 01:31:30 -0300

    For repository purpose

--- app/src/main/java/com/winetraces/nortoncosecha/Config.java
+++ app/src/main/java/com/winetraces/nortoncosecha/Config.java
@@ -12,10 +12,6 @@ import android.widget.TextView;
 
 import java.io.InputStream;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class Config extends AppCompatActivity {
     private ImageView mBackground;
     private String sBackground = null;
